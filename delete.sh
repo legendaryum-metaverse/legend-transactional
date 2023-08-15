5b91897cda80c762edea067a25c599ff28beceb9
Jorge Clavijo <jym272@gmail.com>
2023-08-15 10:02:04 -0300

    3 commit

--- delete.sh
+++ delete.sh
@@ -4,4 +4,4 @@ find . -name "node_modules" -type d -prune -exec rm -rf '{}' +
 find . -name "dist" -type d -prune -exec rm -rf '{}' +
 find . -name ".turbo" -type d -prune -exec rm -rf '{}' +
 
-
+sfapsefioajseiofjasioejf
