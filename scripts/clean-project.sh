0af40cddddd5884c003edd9c491424339e5319eb
Jorge Clavijo <jym272@gmail.com>
2023-08-15 10:50:10 -0300

    feat: linting implemented

---
+++ scripts/clean-project.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Set script to exit on error, unset variables, and fail pipes
+set -eou pipefail
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PARENT_DIRECTORY="${DIR%/*}"
+cd "$PARENT_DIRECTORY"
+
+declare -a directories=("node_modules" "dist" ".turbo")
+
+clean_directories() {
+  local dir
+  for dir in "${directories[@]}"; do
+    # Find and remove directories with the given name
+    find . -name "$dir" -type d -prune -exec rm -rf '{}' +
+    # Print a green checkmark after cleaning the folder
+    echo -e "\033[32m \xE2\x9C\x94\033[0m \033[95m\x1B[3m$dir\x1B[0m \033[3mcleaned\x1B[0m"
+  done
+}
+
+clean_directories
