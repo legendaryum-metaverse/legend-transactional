b36e587eb19af508e0082ffa7ece7ff804dd3d97
Jorge Clavijo <jym272@gmail.com>
2023-08-17 23:20:57 -0300

    fix: npx changeset

--- builders/changesetAction/bundle/index.js
+++ builders/changesetAction/bundle/index.js
@@ -3205,15 +3205,18 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core = __importStar(__nccwpck_require__(41));
 const getReleasePlanMessage_1 = __nccwpck_require__(477);
 try {
+    // const jsonString = "{\"changesets\":[{\"releases\":[{\"name\":\"saga\",\"type\":\"patch\"}],\"summary\":\"asdawd\",\"id\":\"sweet-horses-joke\"}],\"releases\":[{\"name\":\"saga\",\"type\":\"patch\",\"oldVersion\":\"0.0.2\",\"changesets\":[\"sweet-horses-joke\"],\"newVersion\":\"0.0.3\"}]}\n"
     const jsonString = core.getInput('json-string', { required: true });
     console.log('AA', jsonString);
     const releasePlan = JSON.parse(jsonString);
+    console.log('BB', releasePlan);
     const msg = (0, getReleasePlanMessage_1.getReleasePlanMessage)(releasePlan);
+    console.log('CC', msg);
     core.setOutput('release-plan-message', msg);
 }
 catch (error) {
     console.error(error);
-    core.setFailed('Action failed.');
+    // core.setFailed('Action failed.');
 }
 
 
--- builders/changesetAction/src/index.ts
+++ builders/changesetAction/src/index.ts
@@ -2,12 +2,15 @@ import * as core from '@actions/core';
 import { getReleasePlanMessage, ReleasePlan } from './getReleasePlanMessage';
 
 try {
+    // const jsonString = "{\"changesets\":[{\"releases\":[{\"name\":\"saga\",\"type\":\"patch\"}],\"summary\":\"asdawd\",\"id\":\"sweet-horses-joke\"}],\"releases\":[{\"name\":\"saga\",\"type\":\"patch\",\"oldVersion\":\"0.0.2\",\"changesets\":[\"sweet-horses-joke\"],\"newVersion\":\"0.0.3\"}]}\n"
     const jsonString = core.getInput('json-string', { required: true });
     console.log('AA', jsonString);
     const releasePlan = JSON.parse(jsonString) as ReleasePlan;
+    console.log('BB', releasePlan);
     const msg = getReleasePlanMessage(releasePlan);
+    console.log('CC', msg);
     core.setOutput('release-plan-message', msg);
 } catch (error) {
     console.error(error);
-    core.setFailed('Action failed.');
+    // core.setFailed('Action failed.');
 }
