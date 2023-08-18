90a3a14aeab6d34806f99ae5d47b9709b42d9c14
Jorge Clavijo <jym272@gmail.com>
2023-08-18 00:06:53 -0300

    fix: npx changeset

--- builders/changesetAction/src/index.ts
+++ builders/changesetAction/src/index.ts
@@ -1,4 +1,4 @@
-// import * as core from '@actions/core';
+import * as core from '@actions/core';
 import markdownTable from 'markdown-table';
 
 export type VersionType = 'major' | 'minor' | 'patch' | 'none';
@@ -81,16 +81,16 @@ export const getReleasePlanMessage = (releasePlan: ReleasePlan | null) => {
 };
 
 try {
-    const jsonString =
-        '{"changesets":[{"releases":[{"name":"saga","type":"patch"}],"summary":"asdawd","id":"sweet-horses-joke"}],"releases":[{"name":"saga","type":"patch","oldVersion":"0.0.2","changesets":["sweet-horses-joke"],"newVersion":"0.0.3"}]}\n';
-    // const jsonString = core.getInput('json-string', { required: true });
+    // const jsonString =
+    //     '{"changesets":[{"releases":[{"name":"saga","type":"patch"}],"summary":"asdawd","id":"sweet-horses-joke"}],"releases":[{"name":"saga","type":"patch","oldVersion":"0.0.2","changesets":["sweet-horses-joke"],"newVersion":"0.0.3"}]}\n';
+    const jsonString = core.getInput('json-string', { required: true });
     console.log('AA', jsonString);
     const releasePlan = JSON.parse(jsonString) as ReleasePlan;
     console.log('BB', releasePlan);
     const msg = getReleasePlanMessage(releasePlan);
     console.log('CC', msg);
-    // core.setOutput('release-plan-message', msg);
+    core.setOutput('release-plan-message', msg);
 } catch (error) {
     console.error(error);
-    // core.setFailed('Action failed.');
+    core.setFailed('Action failed.');
 }
