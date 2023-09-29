f5f61ba2cd11136a1499bb163924ad4137db0ce4
Jorge Clavijo <jorgeclavijo@legendaryum.com>
2023-09-29 14:41:59 -0300

    feat: cancel sale put nft on sale (#35)

--- packages/legend-transac/src/@types/saga/commands/integrations.ts
+++ packages/legend-transac/src/@types/saga/commands/integrations.ts
@@ -3,21 +3,37 @@
  */
 export const integrationCommands = {
     /**
+     * Command to emit NFTs generated to the client from "legend-integrations".
+     */
+    EmitNft: 'emit_nft',
+    /**
+     * Command to emit cancel sale of NFT
+     */
+    EmitNftCancelSale: 'emit_nft:cancel_sale',
+    /**
+     * Command to emit NFT on sale
+     */
+    EmitNftOnSale: 'emit_nft:on_sale',
+    /**
      * Command to generate images using OpenAI.
      */
     GenerateImages: 'generate_images:openai',
     /**
-     * Command to upload images to cloud storage.
-     */
-    UploadImages: 'upload_images:cloudstorage',
-    /**
-     * Command to save images to internal storage.
+     * Command to save new generated nft image to internal storage.
      */
     PersistsNft: 'persists_nft',
     /**
-     * Command to emit NFTs to the client from "legend-integrations".
+     * Command to persist the cancel sale of the NFT.
      */
-    EmitNft: 'emit_nft'
+    PersistsNftCancelSale: 'persists_nft:cancel_sale',
+    /**
+     * Command to persist the NFT on sale.
+     */
+    PersistsNftOnSale: 'persists_nft:put_nft_on_sale',
+    /**
+     * Command to upload images to cloud storage.
+     */
+    UploadImages: 'upload_images:cloudstorage'
 } as const;
 /**
  * Available commands for the "legend-integrations" microservice.
