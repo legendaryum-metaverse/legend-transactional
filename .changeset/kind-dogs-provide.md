b9a7c0edc5f1830abb85c545fd557bf14c81145c
Jorge Clavijo <jorgeclavijo@legendaryum.com>
2023-10-02 16:22:27 -0300

    Infinite Nacking with fibonnaci delay (#38)
    * feat: nacking with fibonacci strategy
    
    * feat: nacking with fibonacci strategy

---
+++ .changeset/kind-dogs-provide.md
@@ -0,0 +1,5 @@
+---
+'legend-transac': minor
+---
+
+Se diseña nacking sin limite de nackeo con delay siguiendo la serie de fiboncacci
---
+++ packages/legend-transac/src/utils/fibonacci.ts
@@ -0,0 +1,15 @@
+export const fibonacci = (n: number): number => {
+    if (n <= 0) return 0;
+    if (n === 1) return 1;
+
+    let fibPrev = 0;
+    let fibCurrent = 1;
+
+    for (let i = 2; i <= n; i++) {
+        const temp = fibCurrent;
+        fibCurrent += fibPrev;
+        fibPrev = temp;
+    }
+
+    return fibCurrent;
+};
