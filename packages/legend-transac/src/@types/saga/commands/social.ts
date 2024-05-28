500c0a846eedef6e6ee439e31bc1e004cd0ae55f
Lucho <luciano@wisboo.com>
2024-05-28 11:53:48 -0300

    added new command

--- packages/legend-transac/src/@types/saga/commands/social.ts
+++ packages/legend-transac/src/@types/saga/commands/social.ts
@@ -23,7 +23,12 @@ export const socialCommands = {
     /**
      * Command to update the social user's image.
      */
-    UpdateUserImage: 'update_user:image'
+    UpdateUserImage: 'update_user:image',
+    /**
+     * Command to notify the client with websocket.
+     */
+    NotifyClient: 'notify_client',
+
 } as const;
 /**
  * Available commands for the "social" microservice.
