f19b67824ca5849545a3a0dcc79447ab7585bab3
Patricio Poncini <76538747+PatricioPoncini@users.noreply.github.com>
2024-06-12 11:53:19 -0300

    feat: new command 'SavePurchasedResource' in room-inventory and remove 'PurchaseResource' command in room-snapshot (#186)

---
+++ .changeset/fifty-countries-try.md
@@ -0,0 +1,5 @@
+---
+'legend-transactional': patch
+---
+
+new command 'SavePurchasedResource' in room-inventory and remove 'PurchaseResource' command in room-snapshot
--- packages/legend-transac/src/@types/saga/commands/room-inventory.ts
+++ packages/legend-transac/src/@types/saga/commands/room-inventory.ts
@@ -5,7 +5,11 @@ export const roomInventoryCommands = {
     /**
      * Command to decrease the available quantity of a resource during the purchase process.
      */
-    DecreaseAvailableQuantity: 'resource_purchased:decrease_available_quantity'
+    DecreaseAvailableQuantity: 'resource_purchased:decrease_available_quantity',
+    /**
+     * Command to save a purchased resource on user inventory
+     */
+    SavePurchasedResource: 'resource_purchased:save_purchased_resource'
 } as const;
 /**
  * Available commands for the "room-inventory" microservice.
