3b26ae067148e077a061f39b86be9bbcf481cf7b
lucianomiller <luciano.miller.f@gmail.com>
2024-07-01 18:11:11 -0300

    Added new saga update_island_room_template (#222)
    * new saga
    
    ---------
    
    Co-authored-by: Lucho <luciano@wisboo.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: jorge <jorgeclavijo@legendaryum.com>

---
+++ .changeset/eight-carrots-promise.md
@@ -0,0 +1,5 @@
+---
+'legend-transactional': patch
+---
+
+Added new saga update_island_room_template to update data from island rooms in room creators and showcase
--- packages/legend-transac/src/@types/saga/commands/roomCreator.ts
+++ packages/legend-transac/src/@types/saga/commands/roomCreator.ts
@@ -1,7 +1,12 @@
 /**
  * Different commands related to the "room-creator" microservice.
  */
-export const roomCreatorCommands = {} as const;
+export const roomCreatorCommands = {
+    /**
+     * Command to update the island room template.
+     */
+    update_island_room_template: 'update_island_room_template'
+} as const;
 /**
  * Available commands for the "room-creator" microservice.
  */
