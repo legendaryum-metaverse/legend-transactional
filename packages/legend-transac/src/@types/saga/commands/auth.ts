bf124d231834a05b19d39fa17f448c2d60357740
Jorge Clavijo <jym272@gmail.com>
2024-08-01 18:22:30 -0300

    Le 2215 update social picture (#241)
    * feat: update dep
    
    * feat: update user picture steps, delete commence saga update social user image
    
    * feat: changeset

--- packages/legend-transac/src/@types/saga/commands/auth.ts
+++ packages/legend-transac/src/@types/saga/commands/auth.ts
@@ -1,7 +1,12 @@
 /**
  * Different commands related to the "auth" microservice.
  */
-export const authCommands = {} as const;
+export const authCommands = {
+    /**
+     * Command to update the user's picture, use to delete the base64 image from the user.
+     */
+    UpdateUserPicture: 'update_user:picture'
+} as const;
 /**
  * Available commands for the "auth" microservice.
  */
