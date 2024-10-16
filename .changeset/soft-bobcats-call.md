a71f646635f3b4d5f74ccfc31490f33fb7ff3e4e
Jorge Clavijo <jym272@gmail.com>
2024-10-16 11:58:00 -0300

    Rankings reward coins (#270)
    * feat: commence saga and saga step to rewards ranking users
    
    * feat: changeset added

---
+++ .changeset/soft-bobcats-call.md
@@ -0,0 +1,5 @@
+---
+"legend-transactional": patch
+---
+
+reward users with coins, saga step for coins and commence saga
--- packages/legend-transac/src/@types/saga/commands/coins.ts
+++ packages/legend-transac/src/@types/saga/commands/coins.ts
@@ -3,9 +3,13 @@
  */
 export const coinsCommands = {
     /**
-     * Saga used to deduct coins when a user purchase a resource
+     * Saga step to deduct coins when a user purchase a resource
      */
-    DeductCoins: 'resource_purchased:deduct_coins'
+    DeductCoins: 'resource_purchased:deduct_coins',
+    /**
+     * Saga step to reward coins to users based on their rankings
+     */
+    RankingsRewardCoins: 'rankings_users_reward:reward_coins'
 } as const;
 /**
  * Available commands for the "coins" microservice.
