46c4c1aa587c17e4db22a309a3147f0a0010ff44
PatricioPoncini <patricioponcini10@gmail.com>
2024-11-07 11:02:46 -0300

    feat: command to replicate minimal information of the social user

---
+++ .changeset/few-tools-shop.md
@@ -0,0 +1,5 @@
+---
+'legend-transactional': patch
+---
+
+feat: command to replicate minimal information of the social user
--- packages/legend-transac/src/@types/saga/commands/rapid-messaging.ts
+++ packages/legend-transac/src/@types/saga/commands/rapid-messaging.ts
@@ -1,7 +1,12 @@
 /**
  * Different commands related to the "rapid-messaging" microservice.
  */
-export const rapidMessagingCommands = {} as const;
+export const rapidMessagingCommands = {
+    /**
+     * Command to replicate minimal information of the social user once it is created.
+     */
+    ReplicateMinimizedSocialUser: 'replicate_minimized_social_user'
+} as const;
 /**
  * Available commands for the "rapid-messaging" microservice.
  */
