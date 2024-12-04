0559b5869872b506de439729f34fb07de0d10f6b
PatricioPoncini <patricioponcini10@gmail.com>
2024-12-04 12:32:29 -0300

    fix: add saga step to make transfers to winners

--- packages/legend-transac/src/@types/saga/commands/blockchain.ts
+++ packages/legend-transac/src/@types/saga/commands/blockchain.ts
@@ -1,7 +1,12 @@
 /**
  * Different commands related to the "blockchain" microservice.
  */
-export const blockchainCommands = {} as const;
+export const blockchainCommands = {
+    /**
+     * Saga step to make transfers to winners.
+     */
+    TransferRewardToWinners: 'crypto_reward:transfer_reward_to_winners'
+} as const;
 /**
  * Available commands for the "blockchain" microservice.
  */
