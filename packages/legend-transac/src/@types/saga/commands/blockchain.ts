1d8a0d4cadfe794142333e4aeba4821e072341fd
PatricioPoncini <patricioponcini10@gmail.com>
2025-05-15 11:07:40 -0300

    feat: sort steps in alphabetical order

--- packages/legend-transac/src/@types/saga/commands/blockchain.ts
+++ packages/legend-transac/src/@types/saga/commands/blockchain.ts
@@ -3,13 +3,13 @@
  */
 export const blockchainCommands = {
     /**
-     * Saga step to make transfers to winners.
-     */
-    TransferRewardToWinners: 'crypto_reward:transfer_reward_to_winners',
-    /**
      * Saga step to transfer crypto reward to the winner of a mission.
      */
-    TransferMissionRewardToWinner: 'crypto_reward:transfer_mission_reward_to_winner'
+    TransferMissionRewardToWinner: 'crypto_reward:transfer_mission_reward_to_winner',
+    /**
+     * Saga step to make transfers to winners.
+     */
+    TransferRewardToWinners: 'crypto_reward:transfer_reward_to_winners'
 } as const;
 /**
  * Available commands for the "blockchain" microservice.
