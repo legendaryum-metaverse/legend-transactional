9e96ee49b72342cd7ccf540e38e8f1f7ecffa544
jym272 <jym272@users.noreply.github.com>
2025-06-15 22:47:01 -0300

    delete prettier sql

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,19 +1,12 @@
-/** @type {import('prettier-plugin-sql').SqlBaseOptions} */
-const prettierPluginSqlConfig = {
-  language: 'postgresql',
-  keywordCase: 'upper',
-};
-
 /** @type {import('prettier').Config} */
 const prettierConfig = {
-  plugins: ['prettier-plugin-sh', 'prettier-plugin-sql'],
+  plugins: ['prettier-plugin-sh'],
   singleQuote: true,
   printWidth: 120,
 };
 
 const config = {
   ...prettierConfig,
-  ...prettierPluginSqlConfig,
 };
 
 module.exports = config;
